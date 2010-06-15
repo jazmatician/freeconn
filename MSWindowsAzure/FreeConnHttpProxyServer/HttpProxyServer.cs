eac4c9f06ae69498d392fa5d64f2486c3d4a69a9
imxmwu <imxmwu@95125d83-f819-9610-16e7-ea23b9a66399>
2010-06-15 23:02:02 +0000

    Updated Proxy server code.

--- MSWindowsAzure/FreeConnHttpProxyServer/HttpProxyServer.cs
+++ MSWindowsAzure/FreeConnHttpProxyServer/HttpProxyServer.cs
@@ -11,118 +11,123 @@ namespace FreeConnHttpProxyServer
 {
     class HttpProxyServer
     {
-        Socket socketClient;
-        Byte[] readBuff = new Byte[1024];
-        Byte[] tmpBuffer = null;
-        Encoding myEncoding = Encoding.UTF8;
-
-        public HttpProxyServer(Socket socket)
+        public HttpProxyServer(Socket sockClient)
         {
-            this.socketClient = socket;
+            m_sockClient = sockClient;
         }
+        Socket m_sockClient; //, m_sockServer;
+        Byte[] readBuf = new Byte[1024];
+        Byte[] buffer = null;
+        Encoding ASCII = Encoding.ASCII;
 
         public void Run()
         {
-            string strFromRequest = string.Empty;
-
+            string strFromClient = "";
             try
             {
-                int lengthOfIncomingMessage = ReadMessage(this.socketClient, readBuff, ref strFromRequest);
-                Trace.WriteLine("In coming request string is : \n" + strFromRequest);
-
-                if (lengthOfIncomingMessage == 0)
-                {
+                // Read the incoming text on the socket/
+                int bytes = ReadMessage(m_sockClient,
+                                         readBuf, ref strFromClient);
+                // If it's empty, it's an error, so just return.
+                // This will termiate the thread.
+                if (bytes == 0)
                     return;
-                }
-
                 // Get the URL for the connection. The client browser sends a GET command
                 // followed by a space, then the URL, then and identifer for the HTTP version.
                 // Extract the URL as the string betweeen the spaces.
-                int index1 = strFromRequest.IndexOf(' ');
-                int index2 = strFromRequest.IndexOf(' ', index1 + 1);
+                int index1 = strFromClient.IndexOf(' ');
+                int index2 = strFromClient.IndexOf(' ', index1 + 1);
+                string strClientConnection =
+                      strFromClient.Substring(index1 + 1, index2 - index1);
 
-                string strRequestConnection = strFromRequest.Substring(index1 + 1, index2 - 1);
-                Trace.WriteLine("Request destination : \n" + strRequestConnection);
-                Trace.WriteLine("Request from        : \n" + this.socketClient.RemoteEndPoint);
-
-                if (index1 < 0 || index2 < 0)
+                if ((index1 < 0) || (index2 < 0))
                 {
                     throw (new IOException());
                 }
-
-                // Create web request object
-                WebRequest req = WebRequest.Create(strRequestConnection);
+                // Write a messsage that we are connecting.
+                Trace.WriteLine("Connecting to Site " +
+                                   strClientConnection);
+                Trace.WriteLine("Connection from " +
+                                   m_sockClient.RemoteEndPoint);
+                // Create a WebRequest object.
+                WebRequest req = (WebRequest)WebRequest.Create
+                                                     (strClientConnection);
+                // Get the response from the Web site.
                 WebResponse response = req.GetResponse();
+                int BytesRead = 0;
+                Byte[] Buffer = new Byte[32];
+                int BytesSent = 0;
 
-                int bytesRead = 0;
-                Byte[] buffer = new Byte[32];
-                int bytesSend = 0;
+                // Create a response stream object.
+                Stream ResponseStream = response.GetResponseStream();
 
-                Stream responseStream = response.GetResponseStream();
-                bytesRead = responseStream.Read(buffer, 0, buffer.Length);
+                // Read the response into a buffer.
+                BytesRead = ResponseStream.Read(Buffer, 0, 32);
                 StringBuilder strResponse = new StringBuilder("");
-                while (bytesRead != 0)
+                while (BytesRead != 0)
                 {
-                    strResponse.Append(myEncoding.GetString(buffer, 0, buffer.Length));
-                    this.socketClient.Send(buffer, bytesRead, SocketFlags.None);
-                    bytesSend += bytesRead;
-                    bytesRead = responseStream.Read(buffer, 0, buffer.Length);
+                    // Pass the response back to the client
+                    strResponse.Append(Encoding.ASCII.GetString(Buffer,
+                                        0, BytesRead));
+                    m_sockClient.Send(Buffer, BytesRead, 0);
+                    BytesSent += BytesRead;
+                    // Read the next part of the response
+                    BytesRead = ResponseStream.Read(Buffer, 0, 32);
                 }
             }
             catch (FileNotFoundException e)
             {
-                SendErrorPage (404, "File Not Found", e.Message);
+                SendErrorPage(404, "File Not Found", e.Message);
             }
             catch (IOException e)
             {
-                SendErrorPage (503, "Service not available", e.Message);
+                SendErrorPage(503, "Service not available", e.Message);
             }
             catch (Exception e)
             {
-                  SendErrorPage (404, "File Not Found", e.Message);
-                  Trace.WriteLine (e.StackTrace);
-                  Trace.WriteLine (e.Message);
+                SendErrorPage(404, "File Not Found", e.Message);
+                Trace.WriteLine(e.StackTrace);
+                Trace.WriteLine(e.Message);
             }
             finally
             {
                 // Disconnect and close the socket.
-                if (socketClient != null)
+                if (m_sockClient != null)
                 {
-                    if (socketClient.Connected)
+                    if (m_sockClient.Connected)
                     {
-                        socketClient.Close ();
+                        m_sockClient.Close();
                     }
                 }
             }
+            // Returning from this method will terminate the thread.
         }
-
         // Write an error response to the client.
         void SendErrorPage(int status, string strReason, string strText)
         {
-            SendMessage(socketClient, "HTTP/1.0" + " " +
-                         status + " " + strReason + "\n");
-            SendMessage(socketClient, "Content-Type: text/plain" + "\n");
-            SendMessage(socketClient, "Proxy-Connection: close" + "\n");
-            SendMessage(socketClient, "\n");
-            SendMessage(socketClient, status + " " + strReason);
-            SendMessage(socketClient, strText);
+            SendMessage(m_sockClient, "HTTP/1.0" + " " +
+                         status + " " + strReason + "\r\n");
+            SendMessage(m_sockClient, "Content-Type: text/plain" + "\r\n");
+            SendMessage(m_sockClient, "Proxy-Connection: close" + "\r\n");
+            SendMessage(m_sockClient, "\r\n");
+            SendMessage(m_sockClient, status + " " + strReason);
+            SendMessage(m_sockClient, strText);
         }
 
         // Send a string to a socket.
         void SendMessage(Socket sock, string strMessage)
         {
-            tmpBuffer = new Byte[strMessage.Length + 1];
-            int len = myEncoding.GetBytes(strMessage.ToCharArray(),
-                                          0,
-                                          strMessage.Length, tmpBuffer, 0);
-            sock.Send(tmpBuffer, len, 0);
+            buffer = new Byte[strMessage.Length + 1];
+            int len = ASCII.GetBytes(strMessage.ToCharArray(),
+                                      0, strMessage.Length, buffer, 0);
+            sock.Send(buffer, len, 0);
         }
 
         // Read a string from a socket.
-        int ReadMessage(Socket socket, byte[] buf, ref string strMessage)
+        int ReadMessage(Socket sock, byte[] buf, ref string strMessage)
         {
-            int iBytes = socket.Receive(buf, 1024, 0);
-            strMessage = myEncoding.GetString(buf);
+            int iBytes = sock.Receive(buf, 1024, 0);
+            strMessage = Encoding.ASCII.GetString(buf);
             return (iBytes);
         }
     }
